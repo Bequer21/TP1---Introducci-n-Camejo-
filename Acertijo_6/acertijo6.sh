9a9d12118d734687fbee9c6e561cc859b835fe71
bequer <bhuaman@fi.uba.ar>
2024-10-12 10:50:06 -0300

    acertijo 6 completado

---
+++ Acertijo_6/acertijo6.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Los numeros 1, 2 y 9 por X
+# El 0 y 8 por _ (guión bajo)
+
+entrada=$1
+salida=$2
+
+if [ $# -ne 2 ]; then
+    echo "Error: Se requieren exactamente dos argumentos."
+    exit 1
+fi
+
+touch $salida
+if [ ! -s $entrada ];then
+    exit 1
+fi
+
+auxiliar="auxiliar.txt"
+# reemplazar los numeros 1,2 y 9 por 'X' 
+sed -E 's/[1]|[2]|[9]/X/g' $entrada > $auxiliar 
+
+# reemplazar los numeros 0 y 8  por '_'
+sed -E 's/[0]|[8]/_/g' $auxiliar > $salida
+
+rm "$auxiliar"
